e91a18aa6ef6feea0f0cd5f1e7857cea15ee67f2
Cristian Riano <cristian.riano@glovoapp.com>
2022-02-06 19:57:38 +0100

    Add Movie model and tests

---
+++ src/main/java/com/cristianriano/movies/entities/Movie.java
@@ -0,0 +1,11 @@
+package com.cristianriano.movies.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Movie {
+  private long id;
+  private String name;
+}
