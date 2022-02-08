9dc9885b6979ed9f92f24a934f04b952e96fe6aa
Cristian Riano <cristian.riano@glovoapp.com>
2022-02-08 17:07:26 +0100

    Configure logger

--- src/main/java/com/cristianriano/movies/controllers/RestExceptionHandler.java
+++ src/main/java/com/cristianriano/movies/controllers/RestExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.cristianriano.movies.controllers;
 
 import com.cristianriano.movies.errors.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +14,11 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
+  private final static Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
+
   @ExceptionHandler({ NotFoundException.class })
   protected ResponseEntity<Object> handleNotFound(Exception ex, WebRequest request) {
+    logger.error(ex.getMessage());
     return handleExceptionInternal(ex, "Not Found", new HttpHeaders(), HttpStatus.NOT_FOUND, request);
   }
 }
